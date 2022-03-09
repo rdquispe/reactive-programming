170132e9cce35a0999cf1faa7d1e55d580058546
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-03-08 22:15:21 -0500

    Create Subjects.kt

---
+++ hello-reactive/src/main/kotlin/subjects/Subjects.kt
@@ -0,0 +1,26 @@
+package subjects
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.subjects.PublishSubject
+import io.reactivex.rxjava3.subjects.Subject
+
+
+object Subjects {
+    @Throws(InterruptedException::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val src1 = Observable.just(5, 10, 15, 20)
+            .subscribeOn(Schedulers.computation())
+        val src2 = Observable.just(50, 100, 150, 200)
+            .subscribeOn(Schedulers.computation())
+
+        val subject: Subject<Any> = PublishSubject.create()
+        subject.subscribe { e -> println("Subject: $e") } //Observer 1
+
+		src1.subscribe{e -> println(e)}
+		src2.subscribe{e -> println(e)}
+
+        Thread.sleep(9000)
+    }
+}
